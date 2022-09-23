756e5d81c9329a3c4d03f6b24adbe86fbad8332e
Nishant Gupta <askus.swm@gmail.com>
2022-09-23 09:48:30 +0530

    basic controller for web api

---
+++ PropertyPortal/PropertyPortal/Controllers/PropertyDataController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PropertyPortal.Controllers
+{
+    public class PropertyDataController : ApiController
+    {
+        // GET: api/PropertyData
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PropertyData/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PropertyData
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PropertyData/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PropertyData/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
